241b101071b024fc44135bc76c3b6d63b5c7b144
melvin <melvinn721@gmail.com>
2021-10-18 23:25:36 +1300

    added moviedb API request. current issue - too many movies, slow to narrow down selection

--- moviehangman.js
+++ moviehangman.js
@@ -5,12 +5,16 @@ let guessArray = []
 guessCount.innerHTML = guesses;
 
 //make array out of word to guess
-let word = 'guessmememe';
+let word = 'guess me';
 let wordDisplay = document.getElementById('word')
 let wordArray = word.split('');
 
 //wordDisplay.innerHTML = wordArray.join('');
 console.log(wordArray);
+
+//define movie genre and year
+let movieYear = document.querySelector('#movieYear')
+let movieGenre = document.querySelector('#movieGenre')
 
 
 document.addEventListener('keyup', main(), false)
@@ -54,6 +58,43 @@ function render(){
     wordDisplay.innerHTML = preWordDisplay.join('');
 }
 
+//get random id to pass to getMoive function
+function getRandomId(){
+    return Math.floor(Math.random() * 695063);
+}
 
+let randId = getRandomId()
 
+//async function to get random move from moviedb API
+const getMovie = async(randId) => {
+    let movieData
+    const url = `https://api.themoviedb.org/3/movie/${randId}?api_key=c1fa1ff0ae6c682c3923a2aa5172d1ce`
+        //await means it will wait for this part of the code to return before miving forward
+        try {
+            const res = await fetch(url)
+            if (res.success == false) {
+                getMovie(getRandomId())
+            }
+            return res.json()
+            .then((response) => {
+               
+                movieData = response;
+                //check for non-adult movie, popularity > 4, english language;
+                let year = movieData.release_date;
+                if (movieData.adult || movieData.popularity < 4 || movieData.original_language != 'en') {
+                    getMovie(getRandomId());   
+                }
+                word = movieData.original_title;
+                console.log(word);
+                movieYear.innerHTML = movieData.release_date;
 
+            })
+            
+        }  catch (err) {
+            console.log('error')
+        }
+       
+   
+}
+
+getMovie(randId)
