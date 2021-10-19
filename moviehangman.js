647acbb9f53f1104c28a2a2b8a42d0cece5ee184
melvin <melvinn721@gmail.com>
2021-10-19 23:36:36 +1300

    added fetch API for movie title from movieDB

--- moviehangman.js
+++ moviehangman.js
@@ -1,37 +1,79 @@
 //get and update guess count, define guess array which will hold the letter guessed
 let guessCount = document.getElementById('guesses-remaining');
-let guesses = 10;
-let guessArray = []
+let guesses = 8;
+let guessArray = [' ']
 guessCount.innerHTML = guesses;
 
-//make array out of word to guess
-let word = 'guess me';
+let word;
 let wordDisplay = document.getElementById('word')
-let wordArray = word.split('');
+let wordArray = []
 
-//wordDisplay.innerHTML = wordArray.join('');
-console.log(wordArray);
+let endGame = document.getElementById('end-game');
+//async function to get random move from moviedb API
+const getMovie = async() => {
+    let movieData
+    const url = `https://api.themoviedb.org/3/movie/popular?api_key=c1fa1ff0ae6c682c3923a2aa5172d1ce&language=en-US&page=${getRandomPage()}`
+        //await means it will wait for this part of the code to return before miving forward
+        try {
+            const res = await fetch(url)
+            if (res.success == false) {
+                getMovie(getRandomPage())
+            }
+            return res.json()
+            .then((response) => {
+               
+                movieData = response;
+                word = movieData.results[3].title
+                console.log(word)
 
-//define movie genre and year
-let movieYear = document.querySelector('#movieYear')
-let movieGenre = document.querySelector('#movieGenre')
+                return word.toLowerCase();
+            })
+            
+        }  catch (err) {
+            console.log('error')
+        }
+
+}
 
 
-document.addEventListener('keyup', main(), false)
-//main function, 
-function main(){
-    guessLetter()
+//make array out of word to guess
+const getWord = async() => {
+    word = await getMovie();
+    wordArray = word.split('');
+    console.log(wordArray)
     render()
 }
+
+
+guessLetter()
+getWord()
+render()
+
+
+
+
+//main function, 
+// function main(wordArray){
+//     guessLetter();
+//     getMovie();
+//     getWord();
+//     render(wordArray);
+    
+// }
 
 
 //guess function when key is pressed
 function guessLetter() {
     //if letter guessed is in the array of letters in the word then push it into the guess array and rerun render
+    
     document.addEventListener('keydown', (e) => {
+        console.log(e)
+        console.log(guessArray)
+        
         if (wordArray.indexOf(e.key) != -1 && guessArray.indexOf(e.key) == -1) {
             guessArray.push(e.key)
             render();
+            
 
         } else if(guessArray.indexOf(e.key) != -1){
             console.log('already guessed that letter')
@@ -45,56 +87,42 @@ function guessLetter() {
 //render function will decide what letters are on display by checking if the letters in the guess array are in the letters of wordarray
 //if it is then it will show the letter, if not the in will show a placeholder i.e *
 function render(){
-    console.log('again')
+    console.log(wordArray, 'map')
     let preWordDisplay = wordArray.map(letter => {
         console.log(letter)
+        
         if (guessArray.includes(letter)){
             return letter
         } else if(!guessArray.includes(letter)){
             return '*';
         }
+        
     })
-    console.log(preWordDisplay)
+    
     wordDisplay.innerHTML = preWordDisplay.join('');
+    checkWin(preWordDisplay)
+    
 }
 
-//get random id to pass to getMoive function
-function getRandomId(){
-    return Math.floor(Math.random() * 695063);
+function checkWin(wordDisplay){
+    letterArr = wordDisplay;
+    if(!letterArr.includes('*')){
+        endGame.classList.remove('hidden');
+        console.log('win')
+    }
 }
 
-let randId = getRandomId()
-
-//async function to get random move from moviedb API
-const getMovie = async(randId) => {
-    let movieData
-    const url = `https://api.themoviedb.org/3/movie/${randId}?api_key=c1fa1ff0ae6c682c3923a2aa5172d1ce`
-        //await means it will wait for this part of the code to return before miving forward
-        try {
-            const res = await fetch(url)
-            if (res.success == false) {
-                getMovie(getRandomId())
-            }
-            return res.json()
-            .then((response) => {
-               
-                movieData = response;
-                //check for non-adult movie, popularity > 4, english language;
-                let year = movieData.release_date;
-                if (movieData.adult || movieData.popularity < 4 || movieData.original_language != 'en') {
-                    getMovie(getRandomId());   
-                }
-                word = movieData.original_title;
-                console.log(word);
-                movieYear.innerHTML = movieData.release_date;
-
-            })
-            
-        }  catch (err) {
-            console.log('error')
-        }
-       
-   
+//get random page and num to pass to getMovie function
+function getRandomPage(){
+    return Math.floor(Math.random() * 500);
 }
 
-getMovie(randId)
+function getRandomNum(){
+    return Math.floor(Math.random() * 20);
+}
+
+
+document.addEventListener('keyup', guessLetter(), false)
+
+
+
